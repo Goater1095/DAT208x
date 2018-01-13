a0ee92463646d6916bb3e6d11e041befc23e138c
nosign221 <35381547+nosign221@users.noreply.github.com>
2018-01-12 22:03:19 -0600

    Create bitcoinprice.py

---
+++ bitcoinprice.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+np_price = np.array([4718,4367,6458,9907,13800,14098])
+np_date = np.array([1,2,3,4,5,6])
+plt.plot(np_date,np_price)
+
+plt.show()
